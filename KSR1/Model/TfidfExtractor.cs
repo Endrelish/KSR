577ba50da9a7f753762b3894a6cadd311f12e9f0
TagruP <purgat.pawel@gmail.com>
2018-04-08 16:46:08 +0200

    Added tf-idf extractor.

---
+++ KSR1/Model/TfidfExtractor.cs
@@ -0,0 +1,38 @@
+﻿namespace KSR1.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TfidfExtractor : IExtractor
+    {
+        public void FeatureVector(IEnumerable<ReutersMetricObject> reuters)
+        {
+            var wordsIdf = new Dictionary<string, double>();
+            var tfExtractor = new TfExtractor();
+
+            tfExtractor.FeatureVector(reuters);
+            foreach (var reuter in reuters)
+            {
+                foreach (var d in reuter.Vector)
+                {
+                    reuter.Vector[d.Key] = d.Value * Idf(d.Key, reuters, wordsIdf);
+                }
+            }
+        }
+
+        private double Idf(string word, IEnumerable<ReutersMetricObject> reuters, Dictionary<string, double> idf)
+        {
+            if (idf.TryGetValue(word, out var value)) return value;
+            var count = 0;
+            foreach (var reuter in reuters)
+            {
+                if (reuter.SeparatedBody.Contains(word))
+                {
+                    count++;
+                }
+            }
+            return Math.Log10((double)reuters.Count() / (1 + count));
+        }
+    }
+}
